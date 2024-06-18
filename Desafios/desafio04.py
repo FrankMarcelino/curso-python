ee106f0b1af2d27bac455a5056641cbd5968fd87
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-18 09:06:38 -0300

    desafio 04

---
+++ Desafios/desafio04.py
@@ -0,0 +1,4 @@
+algoFoiDgitado = input('digite algo: ')
+
+print(algoFoiDgitado.isalnum() or algoFoiDgitado.isalpha() or algoFoiDgitado.isdecimal() or algoFoiDgitado.isnumeric() or algoFoiDgitado.isprintable() or algoFoiDgitado.isspace() or algoFoiDgitado.isupper() or algoFoiDgitado.islower() or algoFoiDgitado.istitle() or algoFoiDgitado.isupper() or algoFoiDgitado.islower())
+print(type(algoFoiDgitado))
