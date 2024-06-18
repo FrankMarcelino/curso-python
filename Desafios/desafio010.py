ad618a88901850c36d0bf1d105cfdb1670114f87
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-18 10:54:12 -0300

    correção dos desafios 11

--- Desafios/desafio010.py
+++ Desafios/desafio010.py
@@ -1,7 +1,7 @@
 # desafio 10
 
-valorEmReais = float(input('digite um valor em reais: '))
+valorEmReais = float(input('digite um valor em reais: R$'))
 
 valorEmDolar = valorEmReais / 5.42
 
-print('o valor em dolar e: {} dolares'.format(valorEmDolar))
+print('o valor em dolar e: US${:.2f} dolares'.format(valorEmDolar))
