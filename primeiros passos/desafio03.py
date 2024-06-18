19d40e98de8227132282baeac746662c6ef2c7bf
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-17 22:37:49 -0300

    desafio 03 finalizado

---
+++ primeiros passos/desafio03.py
@@ -0,0 +1,6 @@
+#desafio 03
+
+numero01  = input('digite um numero: ')
+numero02  = input('digite outro numero: ')
+soma = int(numero01) + int(numero02)
+print('a soma entre {} e {} e {}'.format(numero01, numero02, soma))
