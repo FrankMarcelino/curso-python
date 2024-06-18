796d56d5d8458c699f26cb823c6b9dcafc95c945
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-18 17:51:23 -0300

    finalizado desafio 14

---
+++ Desafios/desafio014.py
@@ -0,0 +1,7 @@
+# desafio 14
+
+temperaturaEmCelsius = float(input('digite a temperatura em celsius: '))
+
+temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32
+
+print('a temperatura em fahrenheit e: {}°F'.format(temperaturaEmFahrenheit))
