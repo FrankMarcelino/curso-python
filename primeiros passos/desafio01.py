ccc5dd19893b2793d307bb678e0db70f4e0b5751
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-17 22:35:07 -0300

    deafio 02 feito

--- primeiros passos/desafio01.py
+++ primeiros passos/desafio01.py
@@ -1,3 +1,3 @@
-===== Desafio 01 =====
+# Desafio 01
 nome = input('Qual seu nome? ')
 print('Ola {}, prazer em te conhecer!'.format(nome))
---
+++ primeiros passos/desafio02.py
@@ -0,0 +1,6 @@
+# Desafio 02
+dia = input('Qual o dia  voce nasceu? ')
+mes = input('Qual o mes  voce nasceu? ')
+ano = input('Qual o ano  voce nasceu? ')
+
+print('Voce nasceu no dia {} de {} de {}'.format(dia, mes, ano))
