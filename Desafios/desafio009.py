579eba3ed1e4ff7dbed2767d0d78efd98be5735c
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-18 10:39:17 -0300

    correção no layout do desafio09

--- Desafios/desafio009.py
+++ Desafios/desafio009.py
@@ -1,4 +1,13 @@
 #desafio 09
 numeroInteiro = int(input('digite um numero inteiro: '))
 
-print(numeroInteiro * 1  , numeroInteiro * 2  , numeroInteiro * 3  , numeroInteiro * 4  , numeroInteiro * 5  , numeroInteiro * 6  , numeroInteiro * 7  , numeroInteiro * 8  , numeroInteiro * 9  , numeroInteiro * 10)
+print(numeroInteiro, 'x 1 = ', numeroInteiro * 1)
+print(numeroInteiro, 'x 2 = ', numeroInteiro * 2)
+print(numeroInteiro, 'x 3 = ', numeroInteiro * 3)
+print(numeroInteiro, 'x 4 = ', numeroInteiro * 4)
+print(numeroInteiro, 'x 5 = ', numeroInteiro * 5)
+print(numeroInteiro, 'x 6 = ', numeroInteiro * 6)
+print(numeroInteiro, 'x 7 = ', numeroInteiro * 7)
+print(numeroInteiro, 'x 8 = ', numeroInteiro * 8)
+print(numeroInteiro, 'x 9 = ', numeroInteiro * 9)
+print(numeroInteiro, 'x 10 = ', numeroInteiro * 10)
