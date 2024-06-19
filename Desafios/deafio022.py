eb7f59e8dc3d29f6feb030e14dcfeaa794d7fb9d
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-19 16:00:27 -0300

    desafio 22

---
+++ Desafios/deafio022.py
@@ -0,0 +1,14 @@
+# desafio 022
+
+# Faca um programa que leia o nome completo de uma pessoa
+# Mostre o nome com todas as letras maiusculas
+# Mostre o nome com todas as letras minusculas
+# Quantas letras ao todo(sem considerar espacos)
+# Quantas letras no primeiro nome
+
+nome = input('digite seu nome: ').strip()
+
+print('seu nome em maiusculas e: {}'.format(nome.upper()))
+print('seu nome em minusculas e: {}'.format(nome.lower()))
+print('seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
+print('seu primeiro nome tem {} letras'.format(nome.find(' ')))
