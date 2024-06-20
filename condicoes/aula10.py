ea2adca555c7e179d7552ef5be53aac683a16c0e
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-20 08:19:11 -0300

    firs commit the day

--- condicoes/aula10.py
+++ condicoes/aula10.py
@@ -1,6 +1,19 @@
+# primeiro exemplo
 nome = str(input('Qual seu nome? '))
 if nome == 'Frank':
   print('Que nome lindo vc tem!')
 else:
   print('Seu nome é legal')
 print('Bom dia, {}'.format(nome))
+print('--fim do primeiro exemplo--')
+
+# segundo exemplo
+nota1 = float(input('digite a primeira nota: '))
+nota2 = float(input('digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+print('sua media foi {:.1f}'.format(media))
+if media >= 7:
+  print('Voce foi aprovado! PARABENS!')
+else:
+  print('sua media foi ruim! ESTUDE MAIS!')
+print('--fim do segundo exemplo--')
