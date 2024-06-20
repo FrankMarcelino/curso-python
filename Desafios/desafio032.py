4223c2cb05114e54889d552b802dbe35305933a0
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-20 09:56:36 -0300

    atualização do desafio - import datetime

--- Desafios/desafio032.py
+++ Desafios/desafio032.py
@@ -1,8 +1,11 @@
 # desafio 032
 
 # Faca um programa que leia um ano qualquer e mostre se ele e bissexto
+from datetime import date
+ano = int(input('digite um ano ou coloque 0 para analisar o ano atual: '))
 
-ano = int(input('digite um ano: '))
+if ano == 0:
+  ano = date.today().year
 
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
   print('o ano {} e bissexto'.format(ano))
