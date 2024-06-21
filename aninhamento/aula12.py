adfb81dbff59e5bddecb58efc5fecab95f25e449
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-21 15:22:57 -0300

    aula 12

---
+++ aninhamento/aula12.py
@@ -0,0 +1,14 @@
+# aula12
+
+# aninhamento
+
+# exemplo 1
+
+nome = str(input('qual seu nome? '))
+if nome == 'Frank':
+  print('que nome lindo vc tem!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+  print('seu nome e bem popular no Brasil')
+else:
+  print('seu nome é normal')
+print('bom dia, {}'.format(nome))
