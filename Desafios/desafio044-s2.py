88c77fcca363e6e5cea1e1037b39ff13d60983ae
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-21 16:17:45 -0300

    desafio 44 s2

--- Desafios/desafio044-s2.py
+++ Desafios/desafio044-s2.py
@@ -17,3 +17,16 @@ print('KEN')
 sleep(1)
 print('PO!!!')
 print('-=' * 11)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-=' * 11)
+
+if computador == 0: #computador jogou PEDRA
+  if jogador == 0:
+    print('EMPATE')
+  elif jogador == 1:
+    print('JOGADOR VENCE')
+  elif jogador == 2:
+    print('COMPUTADOR VENCE')
+  else:
+    print('JOGADA INVALIDA!')
