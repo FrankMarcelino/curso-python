38e601a564b4a016aee50e1d49aedebfb791828b
frankMarcelino <frank.silva.ti@gmail.com>
2024-06-22 10:57:46 -0300

    finalizado desafio 45

--- Desafios/desafio045-s2.py
+++ Desafios/desafio045-s2.py
@@ -30,3 +30,25 @@ if computador == 0: #computador jogou PEDRA
     print('COMPUTADOR VENCE')
   else:
     print('JOGADA INVALIDA!')
+
+elif computador == 1: #computador jogou PAPEL
+  if jogador == 0:
+    print('COMPUTADOR VENCE')
+  elif jogador == 1:
+    print('EMPATE')
+  elif jogador == 2:
+    print('JOGADOR VENCE')
+  else:
+    print('JOGADA INVALIDA!')
+
+elif computador == 2: #computador jogou TESOURA
+  if jogador == 0:
+    print('JOGADOR VENCE')
+  elif jogador == 1:
+    print('COMPUTADOR VENCE')
+  elif jogador == 2:
+    print('EMPATE')
+  else:
+    print('JOGADA INVALIDA!')
+  
+  
