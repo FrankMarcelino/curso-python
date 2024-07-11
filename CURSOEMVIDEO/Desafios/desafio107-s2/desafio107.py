b4af789616be4010dccbf18e720beef83ebe52ef
frankMarcelino <frank.silva.ti@gmail.com>
2024-07-11 09:31:44 -0300

    desafio 107

---
+++ CURSOEMVIDEO/Desafios/desafio107-s2/desafio107.py
@@ -0,0 +1,42 @@
+import utidadesPreco
+
+print('-' * 30)
+print('{:^30}'.format('verficador de preço'))
+print('-' * 30)
+print('MENU PRINCIPAL')
+print('-' * 30)
+print('[1] - Aumentar')
+print('[2] - Diminuir')
+print('[3] - Dobrar')
+print('[4] - Metade')
+print('[5] - Sair')
+print('-' * 30)
+preco = float(input('Digite o preço: R$'))
+
+while True:
+  print('-' * 30)
+  opcao = int(input('Qual a sua opção? '))
+  if opcao == 1:
+    porcentagem = int(input('Qual a porcentagem? '))
+    utidadesPreco.aumentar(preco, porcentagem)
+  elif opcao == 2:
+    porcentagem = int(input('Qual a porcentagem? '))
+    utidadesPreco.diminuir(preco, porcentagem)
+  elif opcao == 3:
+    utidadesPreco.dobrar(preco)
+  elif opcao == 4:
+    utidadesPreco.metade(preco)
+  elif opcao == 5:
+    break
+  else:
+    print('Opção inválida, tente novamente!')
+  print('-' * 30)
+  print('MENU PRINCIPAL')
+  print('-' * 30)
+  print('[1] - Aumentar')
+  print('[2] - Diminuir')
+  print('[3] - Dobrar')
+  print('[4] - Metade')
+  print('[5] - Sair')
+  print('-' * 30)
+  preco = float(input('Digite o preço: R$'))
