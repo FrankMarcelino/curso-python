621dd4e35e4b2e420581550c8a9860d86c1e48ae
frankMarcelino <frank.silva.ti@gmail.com>
2024-07-11 10:37:31 -0300

    desafio 110

---
+++ CURSOEMVIDEO/Desafios/desafios107-112/desafio110.py
@@ -0,0 +1,5 @@
+# desafio 110
+import utidadesPreco
+
+preco = float(input('digite o preço: R$'))
+utidadesPreco.resumo(preco, 80, 35)
--- CURSOEMVIDEO/Desafios/desafios107-112/utidadesPreco.py
+++ CURSOEMVIDEO/Desafios/desafios107-112/utidadesPreco.py
@@ -17,3 +17,13 @@ def metade(preco=0, formatado=False):
 def moeda(preco=0, moeda='R$'):
   return f'{moeda}{preco:.2f}'.replace('.', ',')
 
+def resumo(preco=0, aumento=0, redução=0):
+  print('-' * 30)
+  print('RESUMO DO VALOR'.center(30))
+  print('-' * 30)
+  print(f'{"Preço analisado:":<20} {moeda(preco)}')
+  print(f'{"Dobro do preço:":<20} {dobrar(preco, True)}')
+  print(f'{"Metade do preço:":<20} {metade(preco, True)}')
+  print(f'{f"{aumento}% de aumento":<20} {aumentar(preco, aumento, True)}')
+  print(f'{f"{redução}% de redução":<20} {diminuir(preco, redução, True)}')
+  print('-' * 30)
